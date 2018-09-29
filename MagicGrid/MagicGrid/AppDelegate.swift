5d938e9c748f8e24a5e4bd98a1daea8df63b69db
SateeshDara <42810065+SateeshDara@users.noreply.github.com>
2018-09-29 15:00:11 +0530

    Add files via upload

---
+++ MagicGrid/MagicGrid/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MagicGrid
+//
+//  Created by Sateesh Dara on 01/09/18.
+//  Copyright © 2018 Sateesh Dara. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
